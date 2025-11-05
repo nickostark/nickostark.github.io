40a3e7efb662477c96590efa36cc24be28c1355f
nickostark <kratsokcin@gmail.com>
2025-11-05 11:02:57 +0100

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 
-const repoName = 'homepage'
+// const repoName = 'homepage'
 
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
