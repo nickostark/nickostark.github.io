a928cf8b89ab59975cfbfad7e85b810311be7ae5
kratsokcin <kratsokcin@gmail.com>
2025-11-05 10:10:22 +0100

    upadated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
+const repoName = 'nickostark.github.io'
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
